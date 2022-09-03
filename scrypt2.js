939129bda3dc7b7bdfdf1bad953de506e7e290f3
Pedro-Arantes <developer9pedro@gmail.com>
2022-09-03 18:31:10 -0300

    tela3 inicio js

--- scrypt2.js
+++ scrypt2.js
@@ -9,6 +9,20 @@ const hideMenu1 = () =>{
     element.classList.add("hide");
     element2.classList.remove("hide");
 
+    const tituloQuizz = document.querySelector("#tela3Titulo")
+    tituloValue = tituloQuizz.value;
+    const urlImage = document.querySelector("#urlImg")
+    imgValue = urlImage.value;
+    const qntPerg = document.querySelector("#qntPerg")
+    qntPergValue = qntPerg.value;
+    const qntNv = document.querySelector("#qntNv")
+    qntNvValue = qntNv.value;
+    const menu2 = document.querySelector("#menu2")
+    const modelo1 = `<div class="caixa-inputs-pergunta"></div>`
+    menu2.innerHTML+= modelo1;
+
+    inserePerguntas();
+
 }
 
 const hideMenu2 = () => {
@@ -45,9 +59,74 @@ const backHome = () =>{
 
 }
 
+const inserePerguntas = () => {
+    
+    const menu2 = document.querySelector("#menu2")
+
+    const caixaPerguntas = document.querySelector(".caixa-inputs-pergunta")
+
+    const modelo = `<div class="pergunta-dividida ">
+    <div>
+        <p class="titulo-dentro-input">Pergunta 1</p>
+    </div>
+    <input placeholder="Texto da pergunta">
+    <input placeholder="Cor de fundo da pergunta">
+    </div>
+
+    <div class="pergunta-dividida ">
+    <div>
+        <p class="titulo-dentro-input">Resposta correta</p>
+    </div>
+    <input placeholder="Resposta correta">
+    <input placeholder="URL da imagem">
+    </div>
+
+    <div class="pergunta-dividida ">
+    <div>
+        <p class="titulo-dentro-input">Respostas incorretas</p>
+    </div>
+    <input placeholder="Resposta incorreta 1">
+    <input placeholder="URL da imagem 1">
+    <input placeholder="Resposta incorreta 2">
+    <input placeholder="URL da imagem 2">
+    <input placeholder="Resposta incorreta 3">
+    <input placeholder="URL da imagem 3">
+    </div>`;
+
+    
+
+    for (let i = 0; i < qntPergValue; i++) {
+        const modelo2 = `<div class=" caixa-inputs-pergunta  edit">Pergunta ${i+1}
+        <ion-icon onclick = "abrePergunta()"  name="create-outline"></ion-icon>
+        </div>`;
+        if (i === 0) {
+            caixaPerguntas.innerHTML+= modelo;
+        }else{
+            menu2.innerHTML+= modelo2;
+        }
+
+        
+        
+    }
+    const modeloBtn = `<button onclick="hideMenu2 ()">Prosseguir pra criar níveis</button>`
+    menu2.innerHTML+= modeloBtn;
+}
+
 //Declaração de Variáveis Globais----------------------------------
 
 let arrayMenu = document.querySelectorAll(".menu")
+
+let tituloValue;
+let imgValue;
+let qntPergValue;
+let qntNvValue;
+
+
+
+
+
+
+
 
 
 
@@ -497,9 +576,10 @@ let arrayMenu = document.querySelectorAll(".menu")
 
 
 
-
-
  function enviarQuiz() {
+
+    const tituloQuizz = document.querySelector("#tela3Titulo")
+    const tituloValue = tituloQuizz.value;
     const parte1 = document.querySelector('.pd0');
     const title = parte1.children[0];
     const url = parte1.children[1];
@@ -533,33 +613,11 @@ let arrayMenu = document.querySelectorAll(".menu")
 
     /* criar nível 2 e 3 no html */
     
-    const objeto = {
-        title: title.value,
-        image: url.value,
-        questions: [
-            {
-                title: perg1.value,
-                color: cor.value,
-                answers: [
-                    {
-                        text: respCerta.value,
-                        image: urlRespCerta.value,
-                        isCorrectAnswer: true
-                    },
-                    {
-                        text: respErrada1.value,
-                        image: urlRespErrada1.value,
-                        isCorrectAnswer: false
-                    }
-                ]
-            },
-            {
-                title: pergunta2queaindanaotem.value,
-                color: "#123456",
-                answers: [
+    
+            
               
     }
 
 
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes', 
-}
+    
+
