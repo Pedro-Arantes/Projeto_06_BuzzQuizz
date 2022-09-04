4ab330cf3d0e70951b33472f82e909e5c86aa3c5
Pedro-Arantes <developer9pedro@gmail.com>
2022-09-04 17:11:17 -0300

    js tela 3 continuação

--- scrypt2.js
+++ scrypt2.js
@@ -11,15 +11,17 @@ const hideMenu1 = () =>{
 
     const tituloQuizz = document.querySelector("#tela3Titulo")
     tituloValue = tituloQuizz.value;
+    tituloQuizz.value = "";
     const urlImage = document.querySelector("#urlImg")
     imgValue = urlImage.value;
+    urlImage.value = "";
     const qntPerg = document.querySelector("#qntPerg")
     qntPergValue = qntPerg.value;
+    qntPerg.value = "";
     const qntNv = document.querySelector("#qntNv")
     qntNvValue = qntNv.value;
-    const menu2 = document.querySelector("#menu2")
-    const modelo1 = `<div class="caixa-inputs-pergunta"></div>`
-    menu2.innerHTML+= modelo1;
+    qntNv.value = "";
+    
 
     inserePerguntas();
 
@@ -31,6 +33,10 @@ const hideMenu2 = () => {
 
     element.classList.add("hide");
     element2.classList.remove("hide");
+
+    
+    capturaPerguntas();
+    insereLevels();
 }
 
 const hideMenu3 = ()  =>{
@@ -39,6 +45,8 @@ const hideMenu3 = ()  =>{
 
     element.classList.add("hide");
     element2.classList.remove("hide");
+
+    insereFinal();
 }
 
 const backHome = () =>{
@@ -57,51 +65,65 @@ const backHome = () =>{
     elementYourQuiz.classList.toggle("hide");
     elementAllQuiz.classList.toggle("hide");
 
+    limpatela()
+
 }
 
 const inserePerguntas = () => {
     
     const menu2 = document.querySelector("#menu2")
 
-    const caixaPerguntas = document.querySelector(".caixa-inputs-pergunta")
+    const modelo4 =`<h1 class="titulo-inputs">Crie suas perguntas</h1>`
+    
+    menu2.innerHTML = modelo4;
 
-    const modelo = `<div class="pergunta-dividida ">
-    <div>
-        <p class="titulo-dentro-input">Pergunta 1</p>
-    </div>
-    <input placeholder="Texto da pergunta">
-    <input placeholder="Cor de fundo da pergunta">
-    </div>
-
-    <div class="pergunta-dividida ">
-    <div>
-        <p class="titulo-dentro-input">Resposta correta</p>
-    </div>
-    <input placeholder="Resposta correta">
-    <input placeholder="URL da imagem">
-    </div>
-
-    <div class="pergunta-dividida ">
-    <div>
-        <p class="titulo-dentro-input">Respostas incorretas</p>
-    </div>
-    <input placeholder="Resposta incorreta 1">
-    <input placeholder="URL da imagem 1">
-    <input placeholder="Resposta incorreta 2">
-    <input placeholder="URL da imagem 2">
-    <input placeholder="Resposta incorreta 3">
-    <input placeholder="URL da imagem 3">
-    </div>`;
+    
 
     
 
     for (let i = 0; i < qntPergValue; i++) {
-        const modelo2 = `<div class=" caixa-inputs-pergunta  edit">Pergunta ${i+1}
-        <ion-icon onclick = "abrePergunta()"  name="create-outline"></ion-icon>
+        let hide = ""
+
+        if (i>0) {
+            hide = "hide"
+        }
+        const modelo = `<div class="caixa-inputs-pergunta ${hide} perg${i+1}">
+        <div class="pergunta-dividida ">
+        <div>
+            <p class="titulo-dentro-input">Pergunta 1</p>
+        </div>
+        <input class="txt${i+1}" placeholder="Texto da pergunta">
+        <input class="cor${i+1}" placeholder="Cor de fundo da pergunta">
+        </div>
+
+        <div class="pergunta-dividida ">
+        <div>
+            <p class="titulo-dentro-input">Resposta correta</p>
+        </div>
+        <input placeholder="Resposta correta">
+        <input placeholder="URL da imagem">
+        </div>
+
+        <div class="pergunta-dividida ">
+        <div>
+            <p class="titulo-dentro-input">Respostas incorretas</p>
+        </div>
+        <input placeholder="Resposta incorreta 1">
+        <input placeholder="URL da imagem 1">
+        <input placeholder="Resposta incorreta 2">
+        <input placeholder="URL da imagem 2">
+        <input placeholder="Resposta incorreta 3">
+        <input placeholder="URL da imagem 3">
+        </div>
         </div>`;
+        
         if (i === 0) {
-            caixaPerguntas.innerHTML+= modelo;
+            menu2.innerHTML+= modelo;
         }else{
+            const modelo2 = `<div class=" caixa-inputs-pergunta  edit">Pergunta ${i+1}
+            <ion-icon onclick = "abrePergunta(this)"  name="create-outline"></ion-icon>
+            </div>
+            ${modelo}`;
             menu2.innerHTML+= modelo2;
         }
 
@@ -111,6 +133,64 @@ const inserePerguntas = () => {
     const modeloBtn = `<button onclick="hideMenu2 ()">Prosseguir pra criar níveis</button>`
     menu2.innerHTML+= modeloBtn;
 }
+
+const insereLevels = ()=>{
+    const modelo = `<div class="caixa-inputs ">
+    <div>
+        <p class="titulo-dentro-input">Nível 1</p>
+    </div>
+    <input placeholder="Título do nível"></input>
+    <input placeholder="% de acerto mínima"></input>
+    <input placeholder="URL da imagem do nível"></input>
+    <div>
+        <textarea placeholder="Descrição do nível"></textarea>
+    </div>
+    </div>`;
+    
+    const menu3 = document.querySelector("#menu3")
+    const modelo1 = `<h1 class="titulo-inputs">Agora, decida os níveis</h1>`
+    menu3.innerHTML = modelo1;
+    
+
+    
+    for (let i = 0; i < qntNvValue; i++) {
+        const modelo2 = `<div class="caixa-inputs  edit">Nível ${i+1}
+        <ion-icon onclick = "abreNível()"  name="create-outline"></ion-icon>
+        </div>`;
+        if (i === 0) {
+            menu3.innerHTML+= modelo;
+        }else{
+            menu3.innerHTML+= modelo2;
+        }
+
+        
+        
+    }
+
+
+    const modeloBtn = `<button onclick="hideMenu3 ()">Finalizar Quizz</button>`
+    menu3.innerHTML+= modeloBtn;
+}
+
+const insereFinal = () => {
+
+    const modelo = `<div class="imagem-comlegenda">
+    <img src="${imgValue}" />
+    <p>${tituloValue}</p>
+    </div>
+    <button>Acessar Quizz</button>
+    <p class="back" onclick="backHome()">Voltar para home</p>`
+    const modelo1 = `<h1 class="titulo-inputs">Seu Quizz está pronto!</h1>`
+    const menu4 = document.querySelector("#menu4")
+    menu4.innerHTML = modelo1;
+    menu4.innerHTML+= modelo;
+}
+
+const capturaPerguntas = ()  =>{
+
+}
+
+
 
 //Declaração de Variáveis Globais----------------------------------
 
